a5c70fee63d6e435a8afd165227252b9b8e83ea2
David Robles <master-nevi@users.noreply.github.com>
2014-09-05 10:46:05 -0700

    First source code commit.

---
+++ HTML/facebook_test_users.js
@@ -0,0 +1,122 @@
+function getParameterByKeyFromQueryString(key, queryString) {
+	key = key.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + key + "=([^&#]*)"),
+	results = regex.exec(queryString);
+	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function showTestUsers(facebookAppID, facebookAppSecret, isMobile) {
+	var appAccessTokenQueryParams =  {"client_id" : facebookAppID, "client_secret" : facebookAppSecret, "grant_type" : "client_credentials"};
+
+	$.ajax({
+		type: "GET",
+		url: "https://graph.facebook.com/oauth/access_token",
+		data: appAccessTokenQueryParams
+	}).done(function(data) {
+		var appAccessToken = getParameterByKeyFromQueryString("access_token", "?" + data);
+
+		var testUserQueryParams =  {"access_token" : appAccessToken};
+
+		$.getJSON('https://graph.facebook.com/' + facebookAppID + '/accounts/test-users', testUserQueryParams, function(data) {
+			var testUserArray = data["data"];
+			// var lastTestUser = $(testUserArray).last()[0];
+			var linkItemArray = [];
+
+			$.each(testUserArray, function(idx, obj){ 
+				var testUserId = obj["id"];
+
+				$.getJSON("https://graph.facebook.com/" + testUserId, testUserQueryParams, function(data) {
+					var email = data["email"];
+					var testUserName = data["name"];
+					var baseURL = isMobile ? 'https://m.facebook.com/login.php' : 'https://facebook.com/login.php'; 
+					var linkItem = $('<li><p><a href="' + baseURL + '?' + $.param({ "email" : email}) + '">Log in as ' + testUserName + '</a></p></li>');
+					linkItem.data("name" , testUserName);
+					linkItemArray.push(linkItem);
+
+					if (testUserArray.length == linkItemArray.length) {						
+						linkItemArray.sort(function(a, b) {
+							var aName = $(a).data("name");
+							var bName = $(b).data("name");
+
+							return aName.localeCompare(bName);
+						});
+
+						var orderedList = $("<ol>");
+						orderedList.append(linkItemArray);
+						$('#main-container').append(orderedList);						
+					};
+
+
+
+
+
+					// var formID = testUserId + 'facebookLoginForm';
+					// var form = $('<form id="' + formID + '" method="post" action="https://m.facebook.com/login.php">\
+					// 	<input type="hidden" name="email" value="' + email + '" />\
+					// 	<input type="hidden" name="pass" value="pass" />\
+					// 	<input type="hidden" name="lsd" value="' + loginToken + '" />\
+					// 	<a onclick="document.getElementById(\'' + formID + '\').submit();">Log in as ' + testUserName + '</a>\
+					// 	</form>');
+					// form.data("name" , testUserName);
+
+					// $('#main-container').append(form);
+
+					// if (lastTestUser["id"] == testUserId) {
+					// 	var list = $("form").get();
+					// 	list.sort(function(a, b) {
+					// 		var aName = $(a).data("name");
+					// 		var bName = $(b).data("name");
+
+					// 		return aName.localeCompare(bName);
+					// 	});
+					// 	for (var i = 0; i < list.length; i++) {
+					// 		list[i].parentNode.appendChild(list[i]);
+					// 	}
+					// };
+				});
+			});
+		});
+	});
+}
+
+function showForm() {
+	$('#main-container').append($('<body>\
+<form name="input" action="" method="get">\
+Facebook App ID: <input type="text" name="facebook_app_id" placeholder="required">\
+<br />\
+Facebook App Secret: <input type="text" name="facebook_app_secret" placeholder="required">\
+<br />\
+<input type="checkbox" name="is_mobile" value="is_mobile">Is Mobile\
+<br />\
+<input type="submit" value="Submit">\
+</form>\
+</body>'));
+}
+
+$(document).ready(function () {
+		// var loginToken = getParameterByName("login_token");
+		// if (!loginToken) {
+		// 	alert('Missing "login_token" query parameter!');
+		// 	return;
+		// };
+		
+		var facebookAppID = getParameterByName("facebook_app_id");
+		var facebookAppSecret = getParameterByName("facebook_app_secret");
+		var isMobile = getParameterByName("is_mobile");
+
+		if (facebookAppID && facebookAppSecret) {
+			showTestUsers(facebookAppID, facebookAppSecret, isMobile);
+		}
+		else {
+			showForm();			
+		}
+
+		
+});
